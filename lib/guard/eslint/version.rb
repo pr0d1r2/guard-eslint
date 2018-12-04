077e339b040e4d78cd44d02987a06aa59257735b
Robin Daugherty <robin@robindaugherty.net>
2018-12-04 10:52:12 -0500

    Release v1.1.0

--- lib/guard/eslint/version.rb
+++ lib/guard/eslint/version.rb
@@ -4,8 +4,8 @@ module Guard
   module EslintVersion
     # http://semver.org/
     MAJOR = 1
-    MINOR = 0
-    PATCH = 1
+    MINOR = 1
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
